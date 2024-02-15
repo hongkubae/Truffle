a3b8946dded527568478cfc9209a179d2ddcd4ff
Hayoung-Im <143571041+Hayoung-Im@users.noreply.github.com>
2024-02-16 06:10:34 +0900

    Update LineGraph.js

--- android/app/components/LineGraph.js
+++ android/app/components/LineGraph.js
@@ -4,10 +4,10 @@ import {Dimensions} from 'react-native';
 import {LineChart} from "react-native-chart-kit";
 import { Dropdown } from 'react-native-element-dropdown';
 import moment from 'moment';
+import firestore from "@react-native-firebase/firestore";
 
 const DropdownData =[
   { label: '최근 1주', value: '1' },
-  { label: '최근 1개월', value: '2' },
   { label: '최근 3개월', value: '3' },
   { label: '최근 6개월', value: '4' },
   { label: '최근 1년', value: '5' },
@@ -21,181 +21,149 @@ const currentMonth = currentDate.month()+1;
 const yearLabels = Array.from({ length: 12 }, (_, index) => moment(currentDate).subtract(11-index, 'month').format('MMM'));
 const halfyearLabels = Array.from({ length: 6 }, (_, index) => moment(currentDate).subtract(5-index, 'month').format('MMM'));
 const quarteryearLabels = Array.from({ length: 3 }, (_, index) => moment(currentDate).subtract(2-index, 'month').format('MMM'));
+const monthlyLabels = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
 
 const LineGraph = () => {
   const [value, setValue] = useState(null);
-   //--드롭다운 선택지에 따라 line chart 출력--\\
-   const renderDropdown = () => {
-     switch(value) {
-       case '1': 
-       return <View>
-        <ScrollView horizontal>
-        <LineChart
-        data={data}
-        width={380}
-        height={220}
-        withVerticalLines={false}
-        chartConfig={chartConfig}
-        bezier
-        />
-        </ScrollView>
-        </View>;
-        break;
-       case '2': 
-       return <View>
-        <LineChart
-        data={data2}
-        width={400}
-        height={220}
-        withVerticalLines={false}
-        chartConfig={chartConfig}
-        bezier
-        />
-        </View>;
-        break;
-       case '3': 
-       return <View>
-       <ScrollView horizontal>
-       <LineChart
-       data={data3}
-       width={450}
-       height={220}
-       withVerticalLines={false}
-       chartConfig={chartConfig}
-       bezier
-       />
-       </ScrollView>
-       </View>;
-        break;
-       case '4': 
-       return <View>
-        <ScrollView horizontal>
-       <LineChart
-       data={data4}
-       width={600}
-       height={220}
-       withVerticalLines={false}
-       chartConfig={chartConfig}
-       bezier
-       />
-       </ScrollView>
-       </View>;
-        break;
-       case '5': 
-       return <View>
-       <ScrollView horizontal>
-       <LineChart
-       data={data5}
-       width={700}
-       height={220}
-       withVerticalLines={false}
-       chartConfig={chartConfig}
-       bezier
-       />
-       </ScrollView>
-       </View>;
+  const [data1, setData1] = useState(null);
+  const [data3, setData3] = useState(null);
+  const [data4, setData4] = useState(null);
+  const [data5, setData5] = useState(null);
 
-        break;
 
-       default: 
-       return <View>
-       <ScrollView horizontal>
-       <LineChart
-       data={data}
-       width={400}
-       height={220}
-       withVerticalLines={false}
-       chartConfig={chartConfig}
-       bezier
-       />
-       </ScrollView>
-       </View>;
-     }
-   }
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const userId = 'xxvkRzKqFcWLVx4hWCM8GgQf1hE3';
+        const userRef = firestore().collection('users').doc(userId);
+        const snapshot = await userRef.get();
+        if (!snapshot.exists) {
+          throw new Error("No monthly expenses data found");
+        }
+        const userData = snapshot.data();
+        const totalExpenses = [];
 
-  return (
-    <View style={styles.container}>
-      <View style={{flexDirection: 'row',}}>
-      <Text style={styles.Texts}>
-        지출 추이
-      </Text>
-      <View style={styles.dropdown_container}>
-      <Dropdown
-        style={styles.dropdown}
-        placeholderStyle={styles.placeholderStyle}
-        selectedTextStyle={styles.selectedTextStyle}
-        data={DropdownData}
-        maxHeight={200}
-        labelField="label"
-        valueField="value"
-        placeholder="최근 1주"
-        value={value}
-        onChange={(item) => {
-          setValue(item.value);
-        }}
-      />
-      </View>
-      </View>
-        {renderDropdown()}
+        for (let i = 0; i < userData.delivery.length; i++) {
+          const totalExpense = (userData.delivery[i] || 0) + (userData.shopping[i] || 0) + (userData.eatOut[i] || 0);
+          totalExpenses.push(totalExpense);
+        }
+
+        setData3({
+          labels: quarteryearLabels,
+          datasets: [
+            {
+              data: [totalExpenses[totalExpenses.length - 3] || 0, totalExpenses[totalExpenses.length - 2] || 0, totalExpenses[totalExpenses.length - 1] || 0],
+              color: (opacity = 1) => `rgba(254, 166, 85, ${opacity})`,
+              strokeWidth: 3
+            }
+          ]
+        });
+
+        setData4({
+          labels: halfyearLabels,
+          datasets: [
+            {
+              data: [totalExpenses[totalExpenses.length - 6] || 0, totalExpenses[totalExpenses.length - 5] || 0, totalExpenses[totalExpenses.length - 4] || 0, totalExpenses[totalExpenses.length - 3] || 0, totalExpenses[totalExpenses.length - 2] || 0, totalExpenses[totalExpenses.length - 1] || 0],
+              color: (opacity = 1) => `rgba(254, 166, 85, ${opacity})`,
+              strokeWidth: 3
+            }
+          ]
+        });
+
+        setData5({
+          labels: yearLabels,
+          datasets: [
+            {
+              data: [totalExpenses[totalExpenses.length - 12] || 0, totalExpenses[totalExpenses.length -11] || 0, totalExpenses[totalExpenses.length - 10] || 0, totalExpenses[totalExpenses.length - 9] || 0, totalExpenses[totalExpenses.length - 8] || 0, totalExpenses[totalExpenses.length - 7] || 0, totalExpenses[totalExpenses.length - 6] || 0, totalExpenses[totalExpenses.length - 5] || 0, totalExpenses[totalExpenses.length - 4] || 0, totalExpenses[totalExpenses.length - 3] || 0, totalExpenses[totalExpenses.length - 2] || 0, totalExpenses[totalExpenses.length - 1] || 0],
+              color: (opacity = 1) => `rgba(254, 166, 85, ${opacity})`,
+              strokeWidth: 3
+            }
+          ]
+        });
+
+        const startDate = moment().startOf('isoWeek'); // 이번 주 시작일
+        const endDate = moment().endOf('isoWeek'); // 이번 주 종료일
+        const days = [];
+        let currentDate = startDate;
+        while (currentDate <= endDate) {
+          const dateStr = currentDate.format('YYYY-MM-DD');
+          const dailyData = await fetchDailyData(userId, dateStr);
+          days.push(dailyData);
+          currentDate = currentDate.clone().add(1, 'days');
+        }
+        setData1({
+          labels: monthlyLabels,
+          datasets: [
+            {
+              data: days,
+              color: (opacity = 1) => `rgba(254, 166, 85, ${opacity})`,
+              strokeWidth: 3
+            }
+          ]
+        });
+        console.log(data1);
+
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    fetchData();
+    fetchDailyData();
+  }, []);
+
+  const fetchDailyData = async (userId, dateStr) => {
+    const docRef = firestore().collection(userId).doc(dateStr);
+    const doc = await docRef.get();
+    if (doc.exists) {
+      return doc.data().amount || 0;
+    } else {
+      return 0;
+    }
+  };
+
+const renderDropdown = () => {
+  switch (value) {
+    case '1':
+      return data1 ? <LineChart data={data1} width={380} height={220} withVerticalLines={false} chartConfig={chartConfig} bezier /> : null;
+    case '3':
+      return data3 ? <LineChart data={data3} width={450} height={220} withVerticalLines={false} chartConfig={chartConfig} bezier /> : null;
+    case '4':
+      return data4 ? <LineChart data={data4} width={450} height={220} withVerticalLines={false} chartConfig={chartConfig} bezier /> : null;
+    case '5':
+      return data5 ? <LineChart data={data5} width={450} height={220} withVerticalLines={false} chartConfig={chartConfig} bezier /> : null;
+    default:
+      return data1 ? <LineChart data={data1} width={400} height={220} withVerticalLines={false} chartConfig={chartConfig} bezier /> : null;
+  }
+};
+
+return (
+  <View style={styles.container}>
+    <View style={{flexDirection: 'row',}}>
+    <Text style={styles.Texts}>
+      지출 추이
+    </Text>
+    <View style={styles.dropdown_container}>
+    <Dropdown
+      style={styles.dropdown}
+      placeholderStyle={styles.placeholderStyle}
+      selectedTextStyle={styles.selectedTextStyle}
+      data={DropdownData}
+      maxHeight={200}
+      labelField="label"
+      valueField="value"
+      placeholder="최근 1주"
+      value={value}
+      onChange={(item) => {
+        setValue(item.value);
+      }}
+    />
     </View>
-  );
+    </View>
+      {renderDropdown()}
+  </View>
+);
 }
-////////////////////그래프//////////////
-const data = {
-  labels: ["Sun","Mon", "Tue", "Wed", "Thu", "Sat",],
-  datasets: [
-    {
-      data: [30, 20, 45, 28, 80, 99, 43],
-      color: (opacity = 1) => `rgba(254, 166, 85, ${opacity})`, // optional
-      strokeWidth: 3 // optional
-    }
-  ],
-};
-
-const data2 = {
-  labels: ["week1","week2", "week3", "week4", ],
-  datasets: [
-    {
-      data: [30, 50, 45, 89, 80,],
-      color: (opacity = 1) => `rgba(254, 166, 85, ${opacity})`, // optional
-      strokeWidth: 3 // optional
-    }
-  ],
-};
-
-const data3 = {
-  labels: quarteryearLabels,
-  datasets: [
-    {
-      data: [130, 210,300],
-      color: (opacity = 1) => `rgba(254, 166, 85, ${opacity})`, // optional
-      strokeWidth: 3 // optional
-    }
-  ],
-};
-
-const data4 = {
-  labels: halfyearLabels,
-  datasets: [
-    {
-      data: [130, 210,300, 250, 110, 280],
-      color: (opacity = 1) => `rgba(254, 166, 85, ${opacity})`, // optional
-      strokeWidth: 3 // optional
-    }
-  ],
-};
-
-const data5 = {
-  labels: yearLabels,
-  datasets: [
-    {
-      data: [130, 210,300, 250, 110, 280, 130, 210,300 ,400, 123, 302],
-      color: (opacity = 1) => `rgba(254, 166, 85, ${opacity})`, // optional
-      strokeWidth: 3 // optional
-    }
-  ],
-};
-//////////////////////////////////////
 
 const styles = StyleSheet.create({
   container: {
